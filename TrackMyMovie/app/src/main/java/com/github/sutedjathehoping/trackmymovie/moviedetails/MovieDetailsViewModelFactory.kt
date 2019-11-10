01b25a4d060c8a7d1342a0557097cea14252ab0f
Sutedja The <tedja_3001@yahoo.com>
2019-11-10 18:02:31 +0700

    Eight Commit

---
+++ TrackMyMovie/app/src/main/java/com/github/sutedjathehoping/trackmymovie/moviedetails/MovieDetailsViewModelFactory.kt
@@ -0,0 +1,19 @@
+package com.github.sutedjathehoping.trackmymovie.moviedetails
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.github.sutedjathehoping.trackmymovie.database.MovieDatabaseDao
+
+class MovieDetailsViewModelFactory (
+    private val movieKey: Long,
+    private val dataSource: MovieDatabaseDao) : ViewModelProvider.Factory {
+
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MovieDetailsViewModel::class.java)) {
+            return MovieDetailsViewModel(movieKey, dataSource) as T
+        }
+        throw IllegalAccessException("ViewModel class not recognized.")
+    }
+
+}
